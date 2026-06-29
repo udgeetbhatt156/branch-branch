12811739d9a7f78d2b75efb881f2ac90913e7073
Udgeet <bhatt8udgeet@gmail.com>
2026-06-29 21:58:25 +0530

    Footer Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import './App.css'
+import Footer from './components/Fotter';
 import Pagination from './components/Pagination';
 
 function App() {
 
 
   return (
+    <>
     <div className="container">
       <h1>Pagination E-Commerce Handler</h1>
       <h2>Welcome to the E-Commerce Pagination Handler</h2>
       <Pagination />
     </div>
+    <Footer/>
+    </>
   )
 }
 
